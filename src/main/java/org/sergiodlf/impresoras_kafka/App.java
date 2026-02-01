38036ba4f948e1acbbfc5d31f88a2be90950596f
Sergio de la Fuente <sergio4vientos@gmail.com>
2026-02-01 12:22:46 +0100

    Iniciar repo

---
+++ src/main/java/org/sergiodlf/impresoras_kafka/App.java
@@ -0,0 +1,10 @@
+package org.sergiodlf.impresoras_kafka;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
